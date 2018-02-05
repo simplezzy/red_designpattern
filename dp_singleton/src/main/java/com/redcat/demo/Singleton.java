d3bc296be10b2c6c99675009b26440cb52306dc4
zhiyu.zhou <zhiyu.zhou@chinaredstar.com>
2018-02-05 21:19:18 +0800

    dp-singleton

--- dp_singleton/src/main/java/com/redcat/demo/Singleton.java
+++ dp_singleton/src/main/java/com/redcat/demo/Singleton.java
@@ -3,5 +3,24 @@ package com.redcat.demo;
 /**
  * Created by zhiyu.zhou on 2018/2/5.
  */
+
+/**
+ * 双重锁校验模式 双重检查锁定才能够正常达到单例效果
+ */
 public class Singleton {
+
+    private volatile static Singleton singleton;
+
+    private Singleton() {}
+
+    public static Singleton getSingleton() {
+        if(null == singleton) {
+            synchronized (Singleton.class) {
+                if(null == singleton) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
 }
